d560f7fd1c7f565ca8db00342e7b8abef9bce548
ingale12345 <ingaleprajawal@.com>
2025-06-03 14:40:05 +0530

    added change for getting user from token

--- src/auth/decorator/get-user-decorator.ts
+++ src/auth/decorator/get-user-decorator.ts
@@ -1,12 +1,18 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Request } from 'express';
 
 export const GetUser = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest() as Request;
+    let request = ctx.switchToHttp().getRequest() as Request;
+    const {
+      user: { password, ...user },
+    }: Request & { user: User } = request as Request & {
+      user: User;
+    };
     if (data) {
-      return request?.user ? request?.user[data] : {};
+      return user ? user[data] : {};
     }
-    return request.user;
+    return user;
   },
 );
