9f4509c131bd228a8bfd6962e58b00c9139a27e5
ingale12345 <ingaleprajawal@.com>
2025-06-04 16:36:11 +0530

    added exception handling for shop creation

--- src/shops/shops.service.ts
+++ src/shops/shops.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
 import { CreateShopDto } from './dto/create-shop.dto';
 import { UpdateShopDto } from './dto/update-shop.dto';
 import { File } from 'multer';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class ShopsService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: CreateShopDto) {
-    return this.prisma.shop.create({
-      data,
-    });
+    try {
+      const shop = await this.prisma.shop.create({
+        data,
+      });
+      return shop;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        Logger.log(error);
+        throw new ConflictException('Email Already Exists.');
+      }
+    }
   }
 
   async findAll() {
